fbc61d5fcacfc5228d5038b2017cd93c2a42e0bf
Aman Ilyasovich <aman.ilyasovich@gmail.com>
2021-04-29 23:35:50 -0700

    Initial Commit

---
+++ Shopily/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shopily
+//
+//  Created by Aman Ilyasovich on 4/29/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shopily/ShopilyApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopilyApp.swift
+//  Shopily
+//
+//  Created by Aman Ilyasovich on 4/29/21.
+//
+
+import SwiftUI
+
+@main
+struct ShopilyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
